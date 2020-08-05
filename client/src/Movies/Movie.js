263a3320e54b0acfb297aca7a092c8d0d5f3e17a
Johnathan Hoyt <johnathanhoyt@Gabriellas-MacBook-Pro.local>
2020-08-05 18:29:00 -0400

    working on MVP, finished routing to individual movies and rendering the movieList

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import {useRouteMatch} from 'react-router-dom';
 const Movie = (props) => {
   const [movie, setMovie] = useState();
- 
+  console.log(movie);
+  console.log(props);
+  const routeMatch = useRouteMatch();
+  console.log(routeMatch);
   useEffect(() => {
-    const id = 1;
+    const id = routeMatch.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
